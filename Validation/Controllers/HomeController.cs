29ffd093d3cf0ff72b5984873d28b557f49a0db2
Taosif Bin Murtaza <taosifbinmurtaza@gmail.com>
2023-03-02 20:02:46 +0600

    Add project files.

---
+++ Validation/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Validation.Models;
+
+namespace Validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Create model)
+        {
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+    }
+}
---
+++ Validation/Models/Create.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Validation.Models
+{
+    public class Create
+    {
+        [Required(ErrorMessage ="*Name is required")]
+        [MinLength(3)]
+        [MaxLength(30)]
+        //[RegularExpression(@"/^[A-Z a-z .-]+$/i")]
+        public string Name { get; set; }
+
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"[0-9]")]
+        public int Id { get; set; }
+
+        [Required]
+       
+        public DateTime Dob { get; set; }
+        [Required]
+        public string Profession { get; set; }
+
+        [Required]
+        public string[] Hobbies { get; set; }
+        public string Gender { get; set; }
+    }
+}
